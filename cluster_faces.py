12860555a71c5ae113667850a3f49f6264b31a9e
Aneesh02 <aneeshtickoo2002@gmail.com>
2021-12-28 19:04:55 +0530

    face detection and clustering scripts added

---
+++ cluster_faces.py
@@ -0,0 +1,58 @@
+# import the necessary packages
+from sklearn.cluster import DBSCAN
+from imutils import build_montages
+import numpy as np
+import argparse
+import pickle
+import cv2
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-e", "--encodings", required=True,
+	help="path to serialized db of facial encodings")
+ap.add_argument("-j", "--jobs", type=int, default=-1,
+	help="# of parallel jobs to run (-1 will use all CPUs)")
+args = vars(ap.parse_args())
+# load the serialized face encodings + bounding box locations from
+# disk, then extract the set of encodings to so we can cluster on
+# them
+print("[INFO] loading encodings...")
+data = pickle.loads(open(args["encodings"], "rb").read())
+data = np.array(data)
+encodings = [d["encoding"] for d in data]
+# cluster the embeddings
+print("[INFO] clustering...")
+clt = DBSCAN(metric="euclidean", n_jobs=args["jobs"])
+clt.fit(encodings)
+# determine the total number of unique faces found in the dataset
+labelIDs = np.unique(clt.labels_)
+numUniqueFaces = len(np.where(labelIDs > -1)[0])
+print("[INFO] # unique faces: {}".format(numUniqueFaces))
+# loop over the unique face integers
+for labelID in labelIDs:
+	# find all indexes into the `data` array that belong to the
+	# current label ID, then randomly sample a maximum of 25 indexes
+	# from the set
+	print("[INFO] faces for face ID: {}".format(labelID))
+	idxs = np.where(clt.labels_ == labelID)[0]
+	idxs = np.random.choice(idxs, size=min(25, len(idxs)),
+		replace=False)
+	# initialize the list of faces to include in the montage
+	faces = []
+	# loop over the sampled indexes
+	for i in idxs:
+		# load the input image and extract the face ROI
+		image = cv2.imread(data[i]["imagePath"])
+		(top, right, bottom, left) = data[i]["loc"]
+		face = image[top:bottom, left:right]
+		# force resize the face ROI to 96x96 and then add it to the
+		# faces montage list
+		face = cv2.resize(face, (96, 96))
+		faces.append(face)
+	# create a montage using 96x96 "tiles" with 5 rows and 5 columns
+	montage = build_montages(faces, (96, 96), (5, 5))[0]
+	
+	# show the output montage
+	title = "Face ID #{}".format(labelID)
+	title = "Unknown Faces" if labelID == -1 else title
+	cv2.imshow(title, montage)
+	cv2.waitKey(0)
---
+++ encode_faces.py
@@ -0,0 +1,46 @@
+# import the necessary packages
+from imutils import paths
+import face_recognition
+import argparse
+import pickle
+import cv2
+import os
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--dataset", required=True,
+	help="path to input directory of faces + images")
+ap.add_argument("-e", "--encodings", required=True,
+	help="path to serialized db of facial encodings")
+ap.add_argument("-d", "--detection-method", type=str, default="hog",
+	help="face detection model to use: either `hog` or `cnn`")
+args = vars(ap.parse_args())
+# grab the paths to the input images in our dataset, then initialize
+# out data list (which we'll soon populate)
+print("[INFO] quantifying faces...")
+imagePaths = list(paths.list_images(args["dataset"]))
+data = []
+# loop over the image paths
+for (i, imagePath) in enumerate(imagePaths):
+	# load the input image and convert it from RGB (OpenCV ordering)
+	# to dlib ordering (RGB)
+	print("[INFO] processing image {}/{}".format(i + 1,
+		len(imagePaths)))
+	print(imagePath)
+	image = cv2.imread(imagePath)
+	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+	# detect the (x, y)-coordinates of the bounding boxes
+	# corresponding to each face in the input image
+	boxes = face_recognition.face_locations(rgb,
+		model=args["detection_method"])
+	# compute the facial embedding for the face
+	encodings = face_recognition.face_encodings(rgb, boxes)
+	# build a dictionary of the image path, bounding box location,
+	# and facial encodings for the current image
+	d = [{"imagePath": imagePath, "loc": box, "encoding": enc}
+		for (box, enc) in zip(boxes, encodings)]
+	data.extend(d)
+# dump the facial encodings data to disk
+print("[INFO] serializing encodings...")
+f = open(args["encodings"], "wb")
+f.write(pickle.dumps(data))
+f.close()
